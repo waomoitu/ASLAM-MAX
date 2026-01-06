d665bcdf7d31996b273218a13a697a06e68bd0a9
waomeotu <hellendizan@gmail.com>
2026-01-06 22:55:50 +0300

    Change BOT_NAME from 'BWM-XMD' to 'ASLAM-MAX'

--- Ibrahim/helper.js
+++ Ibrahim/helper.js
@@ -1,5 +1,5 @@
 const NEWS_LETTER_JID = "120363285388090068@newsletter"; // Replace with your real one
-const BOT_NAME = "BWM-XMD";
+const BOT_NAME = "ASLAM-MAX";
 const DEFAULT_THUMBNAIL = "https://files.catbox.moe/sd49da.jpg";
 
 const createContext = (userJid, options = {}) => ({
